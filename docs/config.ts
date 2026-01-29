cec560ccdb2597a06232b30fbe6bc4f55c6d5aa5
ex_shaohua.liu <ex_shaohua.liu@tcl.com>
2026-01-29 17:32:57 +0800

    初始化

--- docs/config.ts
+++ docs/config.ts
@@ -9,7 +9,34 @@ export default defineConfig({
       { text: "Home", link: "/" },
       { text: "Examples", link: "/markdown-examples" },
     ],
-
+    search: {
+      provider: "local",
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: "搜索文档",
+                buttonAriaLabel: "搜索文档",
+              },
+              modal: {
+                noResultsText: "无法找到相关结果",
+                resetButtonTitle: "清除查询条件",
+                footer: {
+                  selectText: "选择",
+                  navigateText: "切换",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    // 分页
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
     sidebar: [
       {
         text: "Examples",
