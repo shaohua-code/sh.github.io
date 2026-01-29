0373e2fec67370dc9e460da1ebfe4ef54810b7d3
ex_shaohua.liu <ex_shaohua.liu@tcl.com>
2026-01-29 17:20:58 +0800

    初始化

---
+++ docs/config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vitepress";
+export default defineConfig({
+  lang: "zh-CN",
+  title: "VitePress",
+  description: "VitePress - The Vite-powered Static Site Generator",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+      },
+    ],
+  },
+});
